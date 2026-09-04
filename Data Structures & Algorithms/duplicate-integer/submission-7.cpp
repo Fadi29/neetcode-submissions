9e2f3eef539e52ba2e8287fa4d536d5e44a55eff
Fadi-Dev <ff7479664@gmail.com>
2026-09-05 00:53:06 +0300

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    bool hasDuplicate(vector<int>& nums) {
+
+       unordered_set<int> CheckTable;
+
+        CheckTable.reserve(nums.size());
+
+        for (size_t i = 0; i < nums.size(); ++i)
+        {
+          if (CheckTable.find(nums[i]) != CheckTable.end())
+            return true;
+
+           CheckTable.insert(nums[i]);
+       }
+       return false;
+    }
+};
